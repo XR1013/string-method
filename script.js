6fd6ad8c5287e296c3a9155bffdf4332663b1a70
XR1013 <xraptor1013@gmail.com>
2024-09-19 14:00:09 +0900

    DOM요소 안쪽의 텍스트 변경 및 HTML 구조 변경

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("test");
+
+const h1 = document.querySelector("h1");
+h1.innerText = "Test";
+
+const h2 = document.querySelectorAll("h2");
+const linkURL = "https://www.naver.com";
+h2.innerHTML = '<a href =${linkURL}>naver</a>';
